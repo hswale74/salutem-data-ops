0abc4c327cc0501c598818fd43862301ce3ac5a2
Hasnat Swaleheen <hasnatswaleheen@Hasnats-MacBook-Air.local>
2023-08-21 16:44:47 -0500

    Added the root module

---
+++ terraform/resource-groups/ttresources/infra/sql-server-managed-instance/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.0"
+
+  backend "local" {
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0, < 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+}
+
