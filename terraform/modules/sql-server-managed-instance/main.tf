171b3d2e97c426054fd6c488442a916f00adbea5
Hasnat Swaleheen <hasnatswaleheen@Hasnats-MacBook-Air.local>
2023-08-21 17:03:00 -0500

    Renaming

--- terraform/modules/sql-server-managed-instance/main.tf
+++ terraform/modules/sql-server-managed-instance/main.tf
@@ -1,30 +1,30 @@
 
 data "azurerm_resource_group" "this" {
-  name = "TTRESOURCES"
+  name = var.resource_group_name
 }
 
 data "azurerm_network_security_group" "this" {
-  name = "CFSDemoServer-nsg"
+  name = var.network_security_group_name
   resource_group_name = data.azurerm_resource_group.this.name
 }
 
 data azurerm_virtual_network this {
-  name = "TTVirtualNet"
+  name = var.virtual_network_name
   resource_group_name = data.azurerm_resource_group.this.name
 }
 
 data "azurerm_subnet" "this" {
-  name = "WebServers"
+  name = var.subnet_name
   resource_group_name = data.azurerm_resource_group.this.name
   virtual_network_name = data.azurerm_virtual_network.this.name
 }
 
 resource "azurerm_mssql_managed_instance" "main" {
-  name                         = "${var.prefix}-${var.resource_group_name}-${var.environment}"
+  name                         = lower("${var.prefix}-${var.resource_group_name}-${var.environment}")
   resource_group_name          = data.azurerm_resource_group.this.name
   location                     = data.azurerm_resource_group.this.location
   subnet_id                    = data.azurerm_subnet.this.id
-  administrator_login          = "${var.prefix}-${var.resource_group_name}-${var.environment}-admin"
+  administrator_login          = lower("${var.prefix}-${var.resource_group_name}-${var.environment}-admin")
   administrator_login_password = random_password.password.result
   license_type                 = var.license_type
   vcores                       = var.vcores
