933cb0a82c67d7ad2ce5861590937ed29d3717d3
Hasnat Swaleheen <hasnatswaleheen@Hasnats-MacBook-Air.local>
2023-10-18 14:55:43 -0500

    foo

--- openmetadata/satellite/app.py
+++ openmetadata/satellite/app.py
@@ -10,19 +10,18 @@ from metadata.generated.schema.entity.services.connections.metadata.openMetadata
     AuthProvider,
 )
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def health_check():
-    server_config = OpenMetadataConnection(
+server_config = OpenMetadataConnection(
         hostPort=f"{os.getenv('OMETA_DNS')}:8585/api",
         authProvider=AuthProvider("openmetadata"),
         securityConfig=OpenMetadataJWTClientConfig(jwtToken=os.getenv("OMETA_TOKEN")),
     )
-    metadata = OpenMetadata(server_config)
+client = OpenMetadata(server_config)
 
-    return f"Health Check: {metadata.health_check()}"
+app = Flask(__name__)
+
+@app.route("/")
+def health_check(client=client):
+    return f"Health Check: {client.health_check()}"
 
 
 app.run(host="0.0.0.0", port=5000)
